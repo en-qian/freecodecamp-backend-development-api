e9acc45d0d7af8b24d41f309f3373473e899c143
enqian <eqreguse@gmail.com>
2025-08-26 08:19:41 +0800

    Initial commit

---
+++ request-header-parser-microservice/server.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+var express = require('express');
+var app = express();
+
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.socket.remoteAddress,
+    language: req.get('accept-language'),
+    software: req.get('user-agent'),
+  });
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
